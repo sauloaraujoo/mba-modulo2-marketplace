e12404512c991866a57ab141df91acd4bd4f94e1
Diego Junqueira <diego.junq.net@gmail.com>
2025-06-21 20:53:57 -0300

    Arquitetura inicial do projeto usando o Angular 16

---
+++ FrontEnd/Angular/src/app/conta/editar-cliente/editar-cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-cliente',
+  templateUrl: './editar-cliente.component.html',
+  styles: [
+  ]
+})
+export class EditarClienteComponent {
+
+}
