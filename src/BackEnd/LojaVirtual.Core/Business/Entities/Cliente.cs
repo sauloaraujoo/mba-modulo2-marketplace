cfed77275ee11582f3c5c8a640ed80f71c479f2b
Rinaldo Serra <rinaldo.serra@tjse.jus.br>
2025-06-23 21:20:38 -0300

    Adicionado Cliente, Favorito. Atualizado as entidades Produto e Vendedor como novas propriedades e metodos.

---
+++ src/BackEnd/LojaVirtual.Core/Business/Entities/Cliente.cs
@@ -0,0 +1,27 @@
+﻿namespace LojaVirtual.Core.Business.Entities
+{
+    public class Cliente : Entity
+    {
+        protected Cliente() { _favoritos = new List<Favorito>(); }
+        public Cliente(Guid id, string nome, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            _favoritos = new List<Favorito>();
+        }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        private readonly List<Favorito> _favoritos;
+        public IReadOnlyCollection<Favorito> Favoritos => _favoritos;
+        public void AddFavorito(Guid produtoId)
+        {
+            var favorito = new Favorito(Id, produtoId);
+            _favoritos.Add(favorito);
+        }
+        public void RemoveFavorito(Favorito favorito)
+        {
+            _favoritos.Remove(favorito);
+        }
+    }
+}
