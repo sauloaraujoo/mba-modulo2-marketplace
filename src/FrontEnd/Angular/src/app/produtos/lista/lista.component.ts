d00dd8390e7d33c4c1981ef13d6e68e910e35882
Diego Junqueira <diego.junq.net@gmail.com>
2025-06-29 11:13:42 -0300

    criado estrutura para lista de produtos

--- src/FrontEnd/Angular/src/app/produtos/lista/lista.component.ts
+++ src/FrontEnd/Angular/src/app/produtos/lista/lista.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Produto } from '../../models/produto';
+import { ProdutoService } from '../../services/produtos.service';
 
 @Component({
   selector: 'app-lista',
-  templateUrl: './lista.component.html',
-  styles: [
-  ]
+  templateUrl: './lista.component.html'
 })
-export class ListaComponent {
+export class ListaComponent implements OnInit {
+  
+  constructor(private produtoService: ProdutoService) { }
 
+  public produtos: Produto[] = [];
+
+  ngOnInit() {
+    this.produtoService.obterProdutos()
+      .subscribe(
+        produtos => {
+          this.produtos = produtos;
+          console.log(produtos);
+        },
+        error => console.log(error)
+      );
+  }
 }
